01a17a7d86f627771697d5cafcdb98a1d7ad087f
= <=>
2024-07-16 00:09:04 +0100

    Working player movement collision detection

--- Player.cpp
+++ Player.cpp
@@ -19,10 +19,10 @@ void Player::handlePlayerMovement(int direction, Level * level){
             if(!checkWalkCollision(playerX,playerY-16, level)) {this->playerY -= 16;}
             break;
           case 90:
-              if(!checkWalkCollision(playerX + 64, playerY, level)) {this->playerX += 16;}
+              if(!checkWalkCollision(playerX + 16, playerY, level)) {this->playerX += 16;}
             break;
           case 180:
-            if(!checkWalkCollision(playerX,playerY+64, level)) {this->playerY += 16;}
+            if(!checkWalkCollision(playerX,playerY+16, level)) {this->playerY += 16;}
             break;
           case 270:
             if(!checkWalkCollision(playerX -16, playerY, level)) {this->playerX -= 16;}
@@ -31,13 +31,19 @@ void Player::handlePlayerMovement(int direction, Level * level){
 }
 
 bool Player::checkWalkCollision(int x , int y, Level * level){
-    
-    bool collisionOccured = false;
-    int coordsToCheck = (y / 64) * level->mapX + (x/64);
-    if(level->wallMap.at(coordsToCheck) == 1){
-        collisionOccured = true;
-    }
-    return collisionOccured;
+
+    // Check 4 points (x, y-32) (x, y-62) (x+64, y-32)    (x+64, y -64)
+    bool isColliding = false;
+    int XMin = x / 64;
+    int XMax = (x+48) / 64;
+    int YMin = (y +32) / 64;
+    int YMax = (y + 48) / 64;
+    if(level->wallMap.at(YMin*level->mapX+XMin) == 1){isColliding = true;}
+    if(level->wallMap.at(YMin*level->mapX+XMax) == 1){isColliding = true;}
+    if(level->wallMap.at(YMax*level->mapX+XMin) == 1){isColliding = true;}
+    if(level->wallMap.at(YMax*level->mapX+XMax) == 1){isColliding = true;}
+
+    return isColliding;
 }
 
 std::vector<int> Player::getNeighborTiles(std::vector<int> map, int mapX){
