934fb88dc924dffcf2fd78ba5c13768650d97880
= <=>
2024-12-11 23:22:10 +0000

    Sort out entity sizes

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,9 +1,12 @@
 #include "Sprite.h"
 
-Sprite::Sprite(int x, int y, int atlas_height, int atlas_width){
+Sprite::Sprite(int x, int y, int atlas_height, int atlas_width, int height, int width){
     this->atlas_x =  x;
     this->atlas_y = y;
     this->atlas_width = atlas_width;
     this->atlas_height = atlas_height;
+    this->height = height;
+    this->width = width;
+    
 
 }
